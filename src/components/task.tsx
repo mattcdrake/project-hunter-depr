1623d31dfed08b0e05a8b1539d43a51484c3dc92
Matt Drake <drake.mattc@gmail.com>
2021-04-07 19:17:24 -0400

    Include tailwind.css

---
+++ src/components/task.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Task() {
+  return (
+    <p className="border border-black max-w-full flex justify-between">
+      <span>Task Name</span>
+      <span>Due Date</span>
+      <span>★5</span>
+    </p>
+  );
+};
+
+export default Task;
